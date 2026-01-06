02e87b7d806af35a3ef32514eed59a2b24c0030e
Shuvo Roy <sr9891710@gmail.com>
2026-01-06 19:08:15 +0600

    lsat

--- src/components/home/Team.jsx
+++ src/components/home/Team.jsx
@@ -2,78 +2,71 @@ import React from 'react'
 import team from '../../../public/team.json'
 import Slider from 'react-slick';
 function Team() {
-    const settings = {
-        className: "center",
-        // auto
-        dots: true,
-        autoplaySpeed: 2000,
-        cssEase: "linear",
-        slidesToScroll: 1,
-        // auto
-        centerMode: true,
-        infinite: true,
-        centerPadding: "60px",
-        slidesToShow: 2,
-        speed: 500,
-        rows: 2,
-        slidesPerRow: 2,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    centerMode: false,
-                },
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    centerMode: true,
-                    centerPadding: "40px",
-                },
-            },
-            {
-                breakpoint: 300,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    centerMode: true,
-                    centerPadding: "20px",
-                },
-            },
-       
-        ],
-    };
-    return (
-        <div className='p-10'>
-            <div className='text-heder-box'>
-                <h1 className='text-heder-title'>Meet the team</h1>
-                <p className='text-heder-Pera'>A passionate team of digital experts dedicated to your
-                    brand’s success.</p>
-            </div>
-            <div className=''>
-                <Slider {...settings}>
-                    {
-                        team.map((teamCard, index) => (
-                            <div key={index} className='my-3 px-3 '>
-                                <div className='lg:flex gap-4 py-5 border rounded-2xl border-[#695757c0] px-4'>
-                                    <img src={teamCard.img} alt="" className='w-16 h-16 m-auto lg:mr-0 rounded-full object-cover' />
+  const settings = {
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 1280, // laptop
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 1024, // tablet
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768, // large mobile
+        settings: {
+          slidesToShow: 1,
+          centerMode: true,
+          centerPadding: "40px",
+        },
+      },
+      {
+        breakpoint: 480, // Samsung M02s
+        settings: {
+          slidesToShow: 1,
+          centerMode: true,
+          centerPadding: "16px",
+        },
+      },
+    ],
+  };
+  return (
+    <div className='p-10'>
+      <div className='text-heder-box'>
+        <h1 className='text-heder-title'>Meet the team</h1>
+        <p className='text-heder-Pera'>A passionate team of digital experts dedicated to your
+          brand’s success.</p>
+      </div>
+      <div className=''>
+        <Slider {...settings}>
+          {
+            team.map((teamCard, index) => (
+              <div key={index} className='my-3 px-3 '>
+                <div className='lg:flex gap-4 py-5 border rounded-2xl border-[#695757c0] px-4'>
+                  <img src={teamCard.img} alt="" className='w-16 h-16 m-auto lg:mr-0 rounded-full object-cover' />
 
-                                    <div className='text-center lg:text-left'>
-                                        <h1 className='text-lg font-manrope'>{teamCard.name}</h1>
-                                        <p className='text-gray-500'>{teamCard.position}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        ))
-                    }
-                </Slider>
-            </div>
-        </div>
-    )
+                  <div className='text-center lg:text-left'>
+                    <h1 className='text-lg font-manrope'>{teamCard.name}</h1>
+                    <p className='text-gray-500'>{teamCard.position}</p>
+                  </div>
+                </div>
+              </div>
+            ))
+          }
+        </Slider>
+      </div>
+    </div>
+  )
 }
 
 export default Team
